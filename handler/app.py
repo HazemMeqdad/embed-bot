18a58d8d5320019cb3c51cf1bff6d29d085b1841
HazemMeqdad <hazemmeqdad@gmail.com>
2023-05-01 18:36:27 +0300

    Fix embed meta data

--- handler/app.py
+++ handler/app.py
@@ -3,14 +3,16 @@ from discord_interactions.flask_ext import Interactions
 from database.client import UrlsDatabase
 
 CODES = {
-    "title": ["<title>{0}</title>", "<meta content=\"{0}\" property=\"og:title\" />"],
-    "description": ["<meta content=\"{0}\" property=\"og:description\" />"],
-    "url": ["<meta content=\"{0}\" property=\"og:url\" />"],
-    "thumbnail": ["<meta content=\"{0}\" property=\"og:image\" />"],  # small image
-    "color": ["<meta content=\"{0}\" property=\"og:color\" />"],
-    "author": ["<meta content=\"{0}\" property=\"og:author\" />"],
-    "image": ["<meta content=\"{0}\" name=\"twitter:card\" />"],  # large image
+    "title": "<meta content=\"{0}\" property=\"og:title\" />",
+    "image": "<meta content=\"{0}\" property=\"og:image\" />",  # small image
+    "url": "<meta content=\"{0}\" property=\"og:url\" />",
+    "description": "<meta content=\"{0}\" property=\"og:description\" />",
+    "author": "<meta content=\"{0}\" property=\"og:site_name\" />",
+
+    "color": "<meta content=\"{0}\" name=\"theme-color\" />",
 }
+
+int_to_hex = lambda x: '#{0:06X}'.format(x)
 
 class App(Interactions):
     app = Flask(__name__)
@@ -23,17 +25,20 @@ class App(Interactions):
         if not data:
             return ""
         html_code = "<meta name=\"twitter:card\" content=\"summary\">\n"
+        html_code += "<meta property=\"og:type\" content=\"website\">"
         for key, value in data.items():
             if key not in CODES.keys():
                 continue
             if key == "thumbnail":
-                html_code += CODES[key][0].format(value["url"].replace("\"", "\\\"")) + "\n"
+                html_code += CODES[key].format(value["url"].replace("\"", "\\\"")) + "\n"
             elif key == "image":
-                html_code += CODES[key][0].format(value["url"].replace("\"", "\\\"")) + "\n"
+                html_code += CODES[key].format(value["url"].replace("\"", "\\\"")) + "\n"
             elif key == "author":
-                html_code += CODES[key][0].format(value["name"].replace("\"", "\\\"")) + "\n"
+                html_code += CODES[key].format(value["name"].replace("\"", "\\\"")) + "\n"
+            elif key == "color":
+                html_code += CODES[key].format(int_to_hex(value))+ "\n"
             else:
-                html_code += CODES[key][0].format(value.replace("\"", "\\\"") if isinstance(value, str) else value) + "\n"
+                html_code += CODES[key].format(value.replace("\"", "\\\"") if isinstance(value, str) else value) + "\n"
         return html_code
 
     def run(self, *args, **kwargs):
