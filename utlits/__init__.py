e607fa9a5aa7ad57f543b9202d4c9647622ccea9
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2023-05-12 17:16:02 +0300

    add new compiler for video embed

--- utlits/__init__.py
+++ utlits/__init__.py
@@ -1,2 +1,4 @@
 from .logger import CustomFormatter
 from .compiler import compiler_json_data
+from .compiler import video_compiler_json
+from .compiler import videoSchema
--- utlits/compiler.py
+++ utlits/compiler.py
@@ -5,8 +5,34 @@ from typing import List, Union
 import re
 import json
 from typing import List, Union
+from .schema import videoSchema
+import jsonschema
 
-#a better compiler that can get all the json data from strings so you don't need a json code block to get a json data 
+
+def video_compiler_json(strings: List[str]) -> Union[bool, dict]:
+    combined_string = ''.join(strings)
+    start = combined_string.find("{")
+    end = combined_string.rfind("}")
+    if start != -1 and end != -1 and end > start:
+        json_string = combined_string[start:end+1]
+        json_string = re.sub(r',\s*}', '}', json_string)
+        json_string = re.sub(r'"([^"]*)"\s*:\s*"(https?://[^"]+)"', r'"\1": "\2"', json_string)
+        json_string = re.sub(r'"([^"]*)"\s*:\s*"([^"]+)Z"', r'"\1": "\2"', json_string)
+        try:
+            json_data = json.loads(json_string)
+            jsonschema.validate(instance=json_data, schema=videoSchema)
+            return json_data
+        except json.JSONDecodeError as e:
+            print("JSONDecodeError:", str(e))
+            return False
+        except jsonschema.exceptions.ValidationError as e:
+            print("ValidationError", str(e))
+            return False
+        except ValueError as e:
+            print("ValueError:", str(e))
+            return False
+    else:
+        return False    
 
 def compiler_json_data(strings: List[str]) -> Union[bool, dict]:
     combined_string = ''.join(strings)
@@ -32,11 +58,9 @@ def compiler_json_data(strings: List[str]) -> Union[bool, dict]:
     else:
         return False
 
-## a better compiler that use a RegExp to get the embed  but it's need a better regexp
-
 # def compiler_json_data(strings: List[str]) -> Union[bool, dict]:
 #     combined_string = ''.join(strings)
-#     pattern = r"\{[^{}]*\}"
+#     pattern = r"\{(?:[^{}]|(?R))*\}"
 #     match = re.search(pattern, combined_string, re.DOTALL)
 
 #     if match:
