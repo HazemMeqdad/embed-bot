dfc0a484421d2c31fc180e3e93d016d2edfed54f
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2023-06-25 08:36:47 +0300

    some improvements to utlits

--- utlits/__init__.py
+++ utlits/__init__.py
@@ -1,4 +1,4 @@
-from .logger import CustomFormatter
-from .compiler import compiler_json_data
-from .compiler import video_compiler_json
-from .compiler import videoSchema
+from .logger import CustomFormatter;
+from .compiler import videoSchema, json_compiler, embed_schema;
+def int_to_hex(color_int):
+    return f"#{color_int:06X}"
--- utlits/schema.py
+++ utlits/schema.py
@@ -10,3 +10,59 @@ videoSchema = {
     },
     "required": ["title", "description", "video", "width", "height", "image"]
 }
+embed_schema = {
+    "type": "object",
+    "properties": {
+        "title": {"type": "string", "maxLength": 256},
+        "description": {"type": "string", "maxLength": 2048},
+        "url": {"type": "string", "format": "uri"},
+        "color": {"type": "integer"},
+        "fields": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string", "maxLength": 256},
+                    "value": {"type": "string", "maxLength": 1024},
+                    "inline": {"type": "boolean"}
+                },
+                "required": ["name", "value"],
+                "additionalProperties": False
+            }
+        },
+        "image": {
+            "type": "object",
+            "properties": {
+                "url": {"type": "string", "format": "uri"},
+                "proxy_url": {"type": "string", "format": "uri"},
+                "width": {"type": "integer"},
+                "height": {"type": "integer"}
+            },
+            "required": ["url"],
+            "additionalProperties": True
+        },
+        "thumbnail": {
+            "type": "object",
+            "properties": {
+                "url": {"type": "string", "format": "uri"},
+                "proxy_url": {"type": "string", "format": "uri"},
+                "width": {"type": "integer"},
+                "height": {"type": "integer"}
+            },
+            "required": ["url"],
+            "additionalProperties": True
+        },
+        "footer": {
+            "type": "object",
+            "properties": {
+                "text": {"type": "string", "maxLength": 2048},
+                "icon_url": {"type": "string", "format": "uri"},
+                "proxy_icon_url": {"type": "string", "format": "uri"}
+            },
+            "required": ["text"],
+            "additionalProperties": True
+        }
+    },
+    "required": ["title", "description"],
+    "additionalProperties": True
+}
